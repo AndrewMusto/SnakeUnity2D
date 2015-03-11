4f00b0e36786831bb3740198d5e52dc23b43ff94
AndrewMusto <andrew.p.musto@gmail.com>
2015-03-11 18:44:04 -0400

    my changes - added prefab instantiation

--- SnakeUnity2D/Assets/Scripts/GameController.cs
+++ SnakeUnity2D/Assets/Scripts/GameController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public GameObject prefab;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameObject pre = Instantiate (prefab) as GameObject;
 	}
 	
 	// Update is called once per frame
